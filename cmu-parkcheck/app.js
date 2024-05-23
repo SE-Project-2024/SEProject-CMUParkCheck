e548c171d215fb4e93ca69f4c1c2f68d79a547b2
Jireh Mae Muyana <jirehmae_m@cmu.ac.th>
2024-05-23 19:00:35 +0700

    Remove .env file and configured the database.

--- cmu-parkcheck/app.js
+++ cmu-parkcheck/app.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("path");
+const mysql = require("mysql");
+const { connect } = require("http2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Jireh410.",
+    database: "cmu_parkcheck"
+});
+
+connection.connect((err) => {
+    if (err){
+        console.error("Error connecting to database:", err);
+        return;
+    }
+    console.log("Connected to MySQL database");
+});
 
 app.set('view engine', 'ejs');
 
@@ -9,14 +26,42 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'img')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('pages/index');
+});
+
+app.get('/api/parking-areas', (req, res) => {
+    const query = "Select * FROM parking_areas";
+    connect.query(query, (err, results) => {
+        if (err) {
+            console.error("Error executing query: ", err);
+            res.status(500).json({error: "Internal Server Error"});
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.post('/api/feedback', (req, res) => {
+    const { parking_area_id, visit_time, positive_feedback } = req.body;
+    const query = "INSERT INTO feedback (parking_area_id, visit_time, posiitve_feedback) VALUES (?, ?, ?)";
+    connection.query(query, [parking_area_id, visit_time, positive_feedback], (err, results) => {
+        if (err){
+            console.error("Error executing query:", err);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        res.json({ success: true});
+    });
 });
 
 
 const PORT = 3000;
 app.listen(3000, function () {
     console.log(`Server started on port ${PORT}`);
-    console.log("http://localhost:3000/ (home page)");
+    console.log("http://localhost:${PORT}/ (home page)");
 
-})
+});
