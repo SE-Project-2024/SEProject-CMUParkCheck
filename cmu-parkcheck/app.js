f311a20789661f52e8dc564f6831e99271307406
Pitchaya Akkawong <pitchayapugk@gmail.com>
2024-06-22 00:00:07 +0700

    add partials/time.ejs

--- cmu-parkcheck/app.js
+++ cmu-parkcheck/app.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Jireh410.",
+    password: "Kard_1539",
     database: "cmu_parkcheck"
 });
 
