6bf0339bc0a02abf4f7122e2ca78256881901fc9
Jireh Mae Muyana <jirehmae_m@cmu.ac.th>
2024-06-21 22:34:22 +0700

    fixed app.js

--- cmu-parkcheck/app.js
+++ cmu-parkcheck/app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("path");
-const mysql = require("mysql");
-const { connect } = require("http2");
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "Jireh410.",
     database: "cmu_parkcheck"
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err){
-        console.error("Error connecting to database:", err);
+        console.error("Error connecting to database:", err.stack);
         return;
     }
     console.log("Connected to MySQL database");
@@ -52,7 +52,7 @@ app.get('/api/parking-areas', (req, res) => {
 
 app.post('/api/feedback', (req, res) => {
     const { parking_area_id, visit_time, positive_feedback } = req.body;
-    const query = "INSERT INTO feedback (parking_area_id, visit_time, posiitve_feedback) VALUES (?, ?, ?)";
+    const query = "INSERT INTO feedback (parking_area_id, visit_time, positive_feedback) VALUES (?, ?, ?)";
     connection.query(query, [parking_area_id, visit_time, positive_feedback], (err, results) => {
         if (err){
             console.error("Error executing query:", err);
@@ -65,7 +65,7 @@ app.post('/api/feedback', (req, res) => {
 
 
 const PORT = 3000;
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log(`Server started on port ${PORT}`);
     console.log("http://localhost:3000/ (home page)");
     console.log("http://localhost:3000/parking-details");
