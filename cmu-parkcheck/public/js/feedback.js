185f4848d2fdd156b18beaa77848ffed6b7a2de7
Pitchaya Akkawong <pitchayapugk@gmail.com>
2024-06-26 16:41:00 +0700

    like and dislike color

--- cmu-parkcheck/public/js/feedback.js
+++ cmu-parkcheck/public/js/feedback.js
@@ -1,34 +1,3 @@
-// $(document).ready(function() {
-//     let likeCount = 0;
-//     let dislikeCount = 0;
-
-//     function updateMeter() {
-//         const total = likeCount + dislikeCount;
-//         let likePercentage = 50;
-//         let dislikePercentage = 50;
-
-//         if (total > 0) {
-//             likePercentage = (likeCount / total) * 100;
-//             dislikePercentage = (dislikeCount / total) * 100;
-//         }
-
-//         $('#green').css('width', `${likePercentage}%`);
-//         $('#red').css('width', `${dislikePercentage}%`);
-//     }
-
-//     $('.like-icon').click(function() {
-//         likeCount++;
-//         updateMeter();
-//     });
-
-//     $('.dislike-icon').click(function() {
-//         dislikeCount++;
-//         updateMeter();
-//     });
-
-//     // Initial state
-//     updateMeter();
-// });
 $(document).ready(function() {
     let likeCount = 1;
     let dislikeCount = 1;
@@ -49,11 +18,15 @@ $(document).ready(function() {
 
     $('.like-icon').click(function() {
         likeCount++;
+        $(this).addClass('active-like');
+        $('.dislike-icon').removeClass('active-dislike');
         updateMeter();
     });
 
     $('.dislike-icon').click(function() {
         dislikeCount++;
+        $(this).addClass('active-dislike');
+        $('.like-icon').removeClass('active-like'); 
         updateMeter();
     });
 
